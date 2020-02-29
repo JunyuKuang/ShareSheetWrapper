c2936ae2c315c70bb09aaa5a115fbddce292652c
JunyuKuang <lightscreen.app@gmail.com>
2020-02-29 11:21:47 +0800

    Initial Commit

---
+++ ShareSheetWrapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareSheetWrapper
+//
+//  Created by Jonny Kuang on 2/29/20.
+//  Copyright © 2020 Jonny Kuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
