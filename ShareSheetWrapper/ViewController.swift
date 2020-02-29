f9a329c6d53c41b80ebf7223ebbd1533a27651e3
JunyuKuang <lightscreen.app@gmail.com>
2020-02-29 11:29:41 +0800

    Implement ShareViewController

--- ShareSheetWrapper/ViewController.swift
+++ ShareSheetWrapper/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        present(ShareViewController(), animated: true)
+    }
 }
 
+class ShareViewController: UIViewController {
+    
+    private var shareSheet: UIActivityViewController!
+    let customView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://apple.com")!
+        
+        shareSheet = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        
+        addChild(shareSheet)
+        shareSheet.view.frame = view.bounds
+        shareSheet.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(shareSheet.view)
+        shareSheet.didMove(toParent: shareSheet)
+        
+        customView.backgroundColor = .systemBlue
+        view.addSubview(customView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let customViewHeight = view.bounds.height * 0.25
+        let shareSheetHeight = view.bounds.height * 0.75
+        
+        customView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: customViewHeight)
+        shareSheet.view.frame = CGRect(x: 0, y: customViewHeight, width: view.bounds.width, height: shareSheetHeight)
+    }
+}
